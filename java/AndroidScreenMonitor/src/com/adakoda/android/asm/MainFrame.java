cfd310d1eeb1b545de674ee200e928ae1575703a
adakoda.jamjampan@gmail.com <adakoda.jamjampan@gmail.com@446005f7-8002-3569-0e60-7403045a20d3>
2011-02-20 17:46:12 +0000

    Added copyright

--- java/AndroidScreenMonitor/src/com/adakoda/android/asm/MainFrame.java
+++ java/AndroidScreenMonitor/src/com/adakoda/android/asm/MainFrame.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2009-2011 adakoda
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.adakoda.android.asm;
 
 import java.awt.Color;
@@ -580,7 +595,7 @@ public class MainFrame extends JFrame {
 
 		private FBImage getDeviceImage() throws IOException {
 			boolean success = true;
-			boolean debug = false; // リリース時はfalseとすること
+			boolean debug = false;
 			FBImage fbImage = null;
 			RawImage tmpRawImage = null;
 			RawImage rawImage = null;
@@ -595,7 +610,6 @@ public class MainFrame extends JFrame {
 						if (debug == false) {
 							rawImage = tmpRawImage;
 						} else {
-							// デバッグ用に16bppで取得したRAWImageを32bppに書き換える
 							rawImage = new RawImage();
 							rawImage.version = 1;
 							rawImage.bpp = 32;
